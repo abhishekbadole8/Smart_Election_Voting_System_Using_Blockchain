91d3f70f09faf5fd23e02e85254085cdfa1cda88
Rares Stanciu <rares.stanciu@outlook.com>
2022-01-12 18:50:06 +0200

    feat: start voting session

--- hardhat/contracts/VoteSession.sol
+++ hardhat/contracts/VoteSession.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VoteSession is Ownable {
   event CandidateRegistered(uint256 _timestamp, address _address, string _name);
+  event VotingStarted(uint256 _timestamp);
 
   struct Candidate {
     uint256 registered_at;
@@ -21,6 +22,9 @@ contract VoteSession is Ownable {
 
   Status public voteStatus;
   mapping(address => Candidate) public candidates;
+  uint256 public startDate;
+  uint256 public duration;
+  uint8 public numberOfCandidates;
 
   modifier isCandidateRegistrationOpen() {
     require(
@@ -30,22 +34,43 @@ contract VoteSession is Ownable {
     _;
   }
 
-  constructor() {
+  modifier isVotingOpen() {
+    require(voteStatus == Status.VOTING, "Voting is not open.");
+    _;
+  }
+
+  constructor(uint256 _startDate, uint256 _duration) {
     voteStatus = Status.CANDIDATE_REGISTER_OPEN;
+    startDate = _startDate;
+    duration = _duration;
   }
 
   function registerCandidate(address _address, string memory _name)
     external
     isCandidateRegistrationOpen
   {
+    require(
+      candidates[_address].wallet_address == address(0),
+      "Candidate already registered."
+    );
+
     candidates[_address] = Candidate({
       name: _name,
       wallet_address: _address,
       registered_at: block.timestamp
     });
 
+    numberOfCandidates++;
+
     emit CandidateRegistered(block.timestamp, _address, _name);
   }
 
-  
+  function start() external isCandidateRegistrationOpen {
+    require(block.timestamp >= startDate, "Voting cannot start yet.");
+    require(numberOfCandidates > 1, "At least two candidates are necessary.");
+
+    voteStatus = Status.VOTING;
+
+    emit VotingStarted(block.timestamp);
+  }
 }
--- hardhat/test/vote_session.js
+++ hardhat/test/vote_session.js
@@ -1,16 +1,22 @@
 const { expect } = require("chai");
 const { ethers, network } = require("hardhat");
+const { getBlockTimestamp } = require("./utils");
 
 describe("Vote session", function () {
   let contract;
   let owner;
+  let accounts;
 
   beforeEach(async () => {
     [owner] = await ethers.getSigners();
 
     // Deploy VoteSession contract
     const VoteSession = await ethers.getContractFactory("VoteSession");
-    contract = await VoteSession.deploy();
+    const startDate = (await getBlockTimestamp()) + 300; // NOW + 5 minutes
+    const duration = 600; // 10 minutes
+
+    contract = await VoteSession.deploy(startDate, duration);
+
     await contract.deployed();
   });
 
@@ -28,7 +34,6 @@ describe("Vote session", function () {
 
   it("should allow candidate registration after creation", async function () {
     // Register candidate
-    await network.provider.send("evm_setNextBlockTimestamp", [2209969083]);
     const tx = await contract.registerCandidate(
       owner.address,
       "Contract owner"
@@ -39,6 +44,41 @@ describe("Vote session", function () {
     const candidate = await contract.candidates(owner.address);
     expect(candidate.name).to.be.equal("Contract owner");
     expect(candidate.wallet_address).to.be.equal(owner.address);
-    expect(candidate.registered_at).to.be.equal(2209969083);
+    expect(candidate.registered_at).to.be.equal(await getBlockTimestamp());
+  });
+
+  it("should not allow registering twice", async function () {
+    // Register candidate
+    let tx = await contract.registerCandidate(owner.address, "Contract owner");
+    await tx.wait();
+
+    await expect(contract.registerCandidate(owner.address, "New name")).to.be
+      .reverted;
+  });
+
+  it("should allow starting the vote with candidates and after the start date", async () => {
+    const numberOfCandidates = await contract.numberOfCandidates();
+    expect(numberOfCandidates).to.equal(0);
+
+    // Should not be able to start the voting without candidates and before the start date
+    await expect(contract.start()).to.be.reverted;
+
+    // Should not be able to start the voting with candidates but before the start date
+    for (let i = 0; i < 3; i++) {
+      const tx = await contract.registerCandidate(
+        `0x000000000000000000000000000000000000000${i + 1}`,
+        "Contract owner"
+      );
+      await tx.wait();
+      await expect(contract.start()).to.be.reverted;
+    }
+
+    // Should be able to start the voting in optimum conditions
+    await network.provider.send("evm_setNextBlockTimestamp", [
+      (await getBlockTimestamp()) + 300,
+    ]);
+
+    await expect(contract.start()).to.be.ok;
+    expect(await contract.voteStatus()).to.equal(1); // Status.VOTING == 1
   });
 });
